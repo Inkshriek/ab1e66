0b9b35764e580fd9b308a33d2785beae466171d9
Inkshriek <inkshriek@gmail.com>
2022-01-14 00:26:24 -0500

    #3 Front Page Styling Implementation

---
+++ client/src/Welcome.js
@@ -0,0 +1,66 @@
+import {
+    Grid,
+    Box,
+    Typography,
+    Button,
+    FormControl,
+    TextField,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import createPalette from "@material-ui/core/styles/createPalette";
+
+const useStyles = makeStyles((theme) => ({
+    bg: {
+        position: "relative",
+        height: "100vh",
+        backgroundImage: "url(/img/bg-img.png)",
+        backgroundSize: "cover",
+        flex: "35%",
+        zIndex: "0",
+    },
+    fade: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgb(79,145,239,0.8)",
+        zIndex: "1",
+    },
+    container: {
+        position: "relative",
+        top: "30%",
+        zIndex: "2",
+    },
+    text: {
+        textAlign: "center",
+        fontSize: "25px",
+        width: "300px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        color: "white",
+        fontWeight: "lighter",
+    },
+    img: {
+        position: "relative",
+        left: "50%",
+        transform: "translateX(-50%)",
+        marginBottom: "75px",
+        height: "80px",
+        width: "80px",
+    }
+}));
+
+const Welcome = (props) => {
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.bg}>
+            <div className={classes.fade}/>
+            <Box className={classes.container}>
+                <img src="/img/bubble.png" alt="" className={classes.img}/>
+                <Typography variant="h1" className={classes.text}>Converse with anyone with any language</Typography>
+            </Box>
+        </Box>
+    );
+}
+
+export default Welcome;
---
+++ client/src/styles/front.js
@@ -0,0 +1,57 @@
+export const style = {
+    grid: {
+        width: "100%",
+    },
+    main: {
+        flex: "65%",
+        position: "relative",
+        height: "100vh",
+    },
+    container: {
+        maxWidth: "400px",
+        position: "relative",
+        left: "50%",
+        top: "50vh",
+        transform: "translateX(-50%) translateY(-50%)",
+        marginRight: "100px",
+    },
+    formControl: {
+        display: "block",
+    },
+    textbox: {
+        width: "100%",
+        marginTop: "20px",
+    },
+    submit: {
+        left: "50%",
+        transform: "translateX(-50%)",
+        width: "150px",
+        height: "50px",
+        marginTop: "50px",
+        fontSize: "15px"
+    },
+    h1: {
+        fontSize: "25px",
+        fontWeight: "bold",
+    },
+    topToggle: {
+        position: "absolute",
+        width: "100%",
+        top: "20px",
+        paddingRight: "20px",
+        paddingLeft: "20px",
+        justifyContent: "flex-end",
+    },
+    createAccount: {
+        width: "200px",
+        height: "50px",
+        fontSize: "15px",
+    },
+    helperText: {
+        marginRight: "20px",
+        color: "gray",
+        fontSize: "15px",
+        height: "50px",
+        lineHeight: "50px"
+    }
+}
